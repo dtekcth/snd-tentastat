0f2573d1720bf70c43bd824fa3c576af5602aa9d
Christoffer Öjeling <christoffer@ojeling.net>
2013-03-26 22:21:54 +0100

    Generaliserat koden

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-Ska generaliseras...
+# SND Tentastatistik
 
-### Generera PDF
-> lualatex 2012-13lp2.tex
+Genererar PDF-dokument med tentastatistik automatiskt utifrån resultat lagrade i TSV filer med hjälp av LuaLaTeX. Varje latex dokument definierar en tidsperiod att hämta resultat från, förslagsvis en tentavecka.
+
+## Lagringsformat
+Data lagras i TSV-filer (Tab Separated Value).
+Observera att datum måste anges *exakt* enligt [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601): YYYY-MM-DD. 
+
+#### resultat.tsv
+Tentaresultaten i sig. Symbolen "→" används för att betona en tabulator.
+
+* Syntax: `<datum>`→`<kurskod>`→`<antal underkända>`→`<antar 3:or>`→`<antal 4:or>`→`<antal 5:or>`
+* Exempel: `2012-10-20`→`MVE055`→`8`→`19`→`35`→`26`
+
+#### kurskoder.tsv
+Associerar kurskoder med en årskurs (se `program.tsv`) och ett namn.
+
+* Syntax: `<kurskod>`→`<årskurs>`→`<namn>`
+* Exempel: `TDA555`→`1`→`Introduktion till funktionell programmering`
+
+#### program.tsv
+Associerar en årskurssymbol med dess fulla namn. I den genererade PDF:en kommer ordningen på årskurserna att följa denna fil
+
+* Syntax: `<symbol>`→`<namn>`
+* Exempel: `1`→`TKDAT-1`
+
+
+## Skapa ett nytt dokument efter en tentaperiod
+Ytterligare en tentavecka har passerat och det är din uppgift i studienämnden att publicera statistik över resultaten.
+
+1. Lägg till resultat i `resultat.tsv`
+2. Komplettera eventuella saknade kurskoder i `kurskoder.tsv`
+3. Kopiera `2012-13lp1.tex` till `<namn>.tex`och uppdatera:
+    * \title — dokumentets titel
+    * \tvstart — tentaveckans första dag
+    * \tvstop — tenteveckans sista dag
+4. Generera PDF-dokumentet:
+    > lualatex --interaction=nonstopmode `<namn.tex>`
--- functions.lua
+++ functions.lua
@@ -6,7 +6,7 @@ printf = function(s,...)
          end
 
 
-function readTSV (file)
+function readTSV (file, fieldnames)
     local fp = assert(io.open(file, "r"))
     local tsv = {}
     for line in fp:lines() do
@@ -26,7 +26,7 @@ function associateCourses (file)
     local tsv = readTSV(file)
     local look = {}
     for x, v in ipairs(tsv) do
-        look[v[1]] = v[2]
+        look[v[1]] = {["owner"] = v[2], ["name"] = v[3]}
     end
     return look
 end
@@ -37,38 +37,68 @@ fmt = [[\fill[fill=%s] (%f\textwidth,0) rectangle (%f\textwidth,\gheight) node [
 function generate(p1, p2)
     local assoc = associateCourses("kurskoder.tsv")
     local exams = readTSV("resultat.tsv")
+    local program = readTSV("program.tsv")
+
+    res = {}
+    reversemap = {}
+    for i, v in ipairs(program) do
+        reversemap[v[1]] = i
+        res[i] = {}
+    end
+
+
     for _, v in ipairs(exams) do
         local date = v[1]
         if date >= p1 and date <= p2 then
-            local code = v[2]
-            local gradeU = tonumber(v[3])
-            local grade3 = tonumber(v[4])
-            local grade4 = tonumber(v[5])
-            local grade5 = tonumber(v[6])
-            local sum = gradeU + grade3 + grade4 + grade5
-
-            tex.print([[\begin{minipage}{\textwidth}]])
-            printf([[\kurs{%s %s}\par\smallskip]], code, assoc[code])
-
-            tex.print([[\begin{tikzpicture}]])
-            local sU = gradeU / sum
-            local s3 = (gradeU + grade3) / sum
-            local s4 = (gradeU + grade3 + grade4) / sum
-            local mkfield = function(color, a, b, num)
-                printf(fmt, color, a, b, num/sum*100)
-            end
-
-            mkfield("cfail", 0, sU, gradeU)
-            mkfield("cthree", sU, s3, grade3)
-            mkfield("cfour", s3, s4, grade4)
-            mkfield("cfive", s4, 1, grade5)
-
-            tex.print([[\draw (0,0) rectangle (\textwidth,\gheight);]])
-            tex.print([[\end{tikzpicture}]])
-
-            printf([[\par Totalt: %d  --- U: %d ~~~ 3: %d ~~~ 4: %d ~~~ 5: %d]]
-                    , sum, gradeU, grade3, grade4, grade5)
-           tex.print([[\end{minipage}\par\bigskip]])
+            local entry = {}
+            entry.code = v[2]
+            entry.name = assoc[entry.code].name
+            entry.date = date
+            entry.gradeU = tonumber(v[3])
+            entry.grade3 = tonumber(v[4])
+            entry.grade4 = tonumber(v[5])
+            entry.grade5 = tonumber(v[6])
+            local idx = assert(reversemap[assoc[entry.code].owner]
+                , "Kurskod \"" .. assoc[entry.code].owner .. "\" hittades inte" )
+            table.insert(res[idx], entry)
         end
     end
+
+    for i, owner in ipairs(res) do
+        if #owner > 0 then
+            local programname = program[i][2]
+            printf([[\section*{%s}]], programname)
+            for _, course in ipairs(res[i]) do
+                graphCourse(course)
+            end
+        end
+    end
+end
+
+
+function graphCourse(entry)
+    local sum = entry.gradeU + entry.grade3 + entry.grade4 + entry.grade5
+
+    tex.print([[\begin{minipage}{\textwidth}]])
+    printf([[\kurs{%s %s}\par\smallskip]], entry.code, entry.name)
+
+    tex.print([[\begin{tikzpicture}]])
+    local sU = entry.gradeU / sum
+    local s3 = (entry.gradeU + entry.grade3) / sum
+    local s4 = (entry.gradeU + entry.grade3 + entry.grade4) / sum
+    local mkfield = function(color, a, b, num)
+        printf(fmt, color, a, b, num/sum*100)
+    end
+
+    mkfield("cfail", 0, sU, entry.gradeU)
+    mkfield("cthree", sU, s3, entry.grade3)
+    mkfield("cfour", s3, s4, entry.grade4)
+    mkfield("cfive", s4, 1, entry.grade5)
+
+    tex.print([[\draw (0,0) rectangle (\textwidth,\gheight);]])
+    tex.print([[\end{tikzpicture}]])
+
+    printf([[\par Totalt: %d  --- U: %d ~~~ 3: %d ~~~ 4: %d ~~~ 5: %d]]
+            , sum, entry.gradeU, entry.grade3, entry.grade4, entry.grade5)
+   tex.print([[\end{minipage}\bigbreak]])
 end
